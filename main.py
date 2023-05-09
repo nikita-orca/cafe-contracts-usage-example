c59883b452b4ca26c18019ba2fb933047c902c5d
Nick Yasnogorodskyi <nikita@orca.security>
2023-05-09 02:12:15 +0200

    feat: complete contract enforcement and testing

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
 import faust
+from orca.contracts.coffee_ordered import CoffeeOrdered
+from orca.contracts.contract_utils import ContractDetails, ContractIdentifier
+from orca.contracts.order_instructions_recieved import \
+    OrderInstructionsRecieved
 
 app = faust.App("cafe", broker="kafka:9092", value_serializer="json")
 
-ORDERS_INPUT_TOPIC = app.topic("internal_unclear_topic_name")
-INSTRUCTIONS_OUTPUT_TOPIC = app.topic("another_hebrish_topic_name")
+ORDER_INPUT_CONTRACT_ID = ContractIdentifier(
+    domain="cafe",
+    contract_name="coffee_ordered",
+    contract_version=1,
+)
+ORDER_INSTRUCTIONS_OUTPUT_CONTRACT_ID = ContractIdentifier(
+    domain="cafe",
+    contract_name="order_instructions_recieved",
+    contract_version=1,
+)
+
+ORDERS_INPUT_TOPIC = app.topic(
+    ContractDetails(ORDER_INPUT_CONTRACT_ID).get_channel_name(include_env_prefix=True)
+)
+INSTRUCTIONS_OUTPUT_TOPIC = app.topic(
+    ContractDetails(ORDER_INSTRUCTIONS_OUTPUT_CONTRACT_ID).get_channel_name(
+        include_env_prefix=True
+    )
+)
 
 
 @app.agent(ORDERS_INPUT_TOPIC)
 async def process_order(order_stream):
     async for order_dict in order_stream:
+        order = CoffeeOrdered.parse_obj(order_dict)
         info_for_barista = "Take money first and prioritize premium clients instead. "
         use_expensive_seeds = False
         time_limit_in_minutes = 10
 
-        if order_dict["premium_service"]:
+        if order.premium_service:
             info_for_barista = "Be friendly and put a name on a coffee cup. "
             time_limit_in_minutes = 2
             use_expensive_seeds = True
 
-        info_for_barista += f"Prepare {order_dict['coffee_type']}"
+        info_for_barista += f"Prepare {order.coffee_type}"
 
         await INSTRUCTIONS_OUTPUT_TOPIC.send(
-            value=dict(
-                customer_id=order_dict["customer_id"],
+            value=OrderInstructionsRecieved(
+                customer_id=order.customer_id,
                 info_for_barista=info_for_barista,
                 time_limit_in_minutes=time_limit_in_minutes,
                 use_expensive_seeds=use_expensive_seeds,
-            )
+            ).dict()
         )
 
 
---
+++ tests/test_contract.py
@@ -0,0 +1,65 @@
+import asyncio
+from asyncio.events import AbstractEventLoop
+from typing import Generator
+
+import faust
+import pytest
+from orca.contracts.mocker_client.v1.mocker_client import (ContractMock,
+                                                           MockerClient)
+
+from main import (ORDER_INPUT_CONTRACT_ID,
+                  ORDER_INSTRUCTIONS_OUTPUT_CONTRACT_ID, app)
+
+mocker_client = MockerClient(address="localhost", port=8001)
+
+
+@pytest.fixture()
+def order_input_contract_mock() -> Generator[ContractMock, None, None]:
+    mock = ContractMock(
+        contract=ORDER_INPUT_CONTRACT_ID,
+        client=mocker_client,
+    )
+
+    mock.create_channel_if_not_exists()
+    yield mock
+    mock.delete_channel_if_exists()
+
+
+@pytest.fixture()
+def order_instructions_output_mock() -> Generator[ContractMock, None, None]:
+    mock = ContractMock(
+        contract=ORDER_INSTRUCTIONS_OUTPUT_CONTRACT_ID,
+        client=mocker_client,
+    )
+    mock.create_channel_if_not_exists()
+    yield mock
+    mock.delete_channel_if_exists()
+
+
+@pytest.mark.asyncio()
+@pytest.fixture()
+def event_loop() -> Generator[AbstractEventLoop, None, None]:
+    """necessary for testing Faust agents"""
+    yield app.loop
+
+
+@pytest.mark.asyncio()
+async def test_contract_coffee_order_service(
+    order_input_contract_mock: ContractMock,
+    order_instructions_output_mock: ContractMock,
+) -> None:
+    """
+    1. Produce messages to input topic using contract mock
+    2. Spin up service and start processing
+    3. Use output contract mock to verify messages are valid
+    """
+    # 1.
+    await order_input_contract_mock.async_produce()
+    # 2.
+    worker = faust.Worker(app, loglevel="INFO")
+    try:
+        await asyncio.wait_for(worker.start(), 10)
+    except asyncio.exceptions.TimeoutError:
+        await worker.stop()
+    # 3.
+    await order_instructions_output_mock.async_consume(number_of_expected_messages=3)
